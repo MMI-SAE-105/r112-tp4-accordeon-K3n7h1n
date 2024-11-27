f0556900a21250176ddef86d4cdabddc91115925
Enzo Locatelli <enzort70290@gmail.com>
2024-11-27 14:46:53 +0100

    ajout de code js pour empecher la fermeture hors des images

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,14 @@
 // *** ACCORDEON ***
 
-
 const accordeon = document.querySelector(".accordeon");
 accordeon.addEventListener("click", (e) => {
-accordeon.querySelectorAll("details").forEach((det) => (det.open = false));})
+accordeon.querySelectorAll("details").forEach((det) => {
+    det.querySelector("summary").addEventListener("click", (e) => {
+        accordeon.querySelectorAll("details").forEach((autreDet) => {
+            if (autreDet !== det) {
+                autreDet.open = false;
+        }
+        });
+    });
+});
+});
